d1b4082c7f3972bb44271df7756979050fae32d7
Rey Cruz <reycruz@Reys-MacBook-Pro.local>
2017-10-08 08:19:19 -0700

    everything works, except, how to restart after each win/lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,7 @@ var wins = 0;
 var losses = 0;
 var guessesLeft = 12; 
 var guessedLetters = [];
-// var letterToGuess =
+
 
 
 var currentLetter = letters[Math.floor(Math.random() * letters.length)];
@@ -14,31 +14,36 @@ console.log(currentLetter);
 document.onkeyup = function() {
     var userguess = String.fromCharCode(event.keyCode).
     toLowerCase();
+    guessedLetters.push(userguess);
     
     console.log(userguess);
+
+    //for loop
+    // for (var i = 0; i < guessesLeft.length; i++){
 
     if (userguess === currentLetter) {
       wins++;
       guessesLeft = 12;
-      alert(wins)
+      guessedLetters.length = 0;
     }
-    else if (userguess !== currentLetter) {
-      losses++;
-      alert(losses)
-      {
-        if (guessesLeft - 1 === 0);
-      }
-    }
-    if (guessesLeft === 0) {
+    else if (guessesLeft === 0) {
       losses++;
       guessesLeft = 12;
+      guessedLetters.length = 0;
+    }
+    else if (userguess !== currentLetter) {
+      
+      guessesLeft--;
+
     }
 
-   var html = "<p>Guess what letter I'm thinking of:</p>" + 
-   "<p>Wins: " + wins + "</p>"+ 
-   "<p>Losses: "+ losses + "</p>" +
-   "<p>Guesses Left: " + guessesLeft + "</p>" +
-   "<p>Guessed Letter: " +  guessedLetters; + "</p>";
+   var html = "<h1>Psychic Game</h1>" +
+   "<h4>Guess what letter I'm thinking of</h4>" + 
+   "<h3>Wins: " + wins + "</h3>"+ 
+   "<h3>Losses: "+ losses + "</h3>" +
+   "<h3>Guesses left: " + guessesLeft + "</h3>" +
+   "<p>Your guesses so far: " +  guessedLetters; + "</p>";
+
     document.querySelector("#psychicGame").innerHTML = html;
     }
 
