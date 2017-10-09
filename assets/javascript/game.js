be900e84fc7719f343774cde26047c16364e67bb
Rey Cruz <reycruz@Reys-MBP.attlocal.net>
2017-10-08 21:42:19 -0700

    missing how to reset game after win/lose

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,8 @@
 //vars 
-var letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var wins = 0;
 var losses = 0;
-var guessesLeft = 12; 
+var guessesLeft = 12;
 var guessedLetters = [];
 
 
@@ -11,39 +11,47 @@ var currentLetter = letters[Math.floor(Math.random() * letters.length)];
 
 console.log(currentLetter);
 
+
 document.onkeyup = function() {
     var userguess = String.fromCharCode(event.keyCode).
     toLowerCase();
     guessedLetters.push(userguess);
-    
+
     console.log(userguess);
 
-    //for loop
-    // for (var i = 0; i < guessesLeft.length; i++){
 
-    if (userguess === currentLetter) {
-      wins++;
-      guessesLeft = 12;
-      guessedLetters.length = 0;
-    }
-    else if (guessesLeft === 0) {
-      losses++;
-      guessesLeft = 12;
-      guessedLetters.length = 0;
-    }
-    else if (userguess !== currentLetter) {
-      
-      guessesLeft--;
 
-    }
+if (userguess === currentLetter) {
+    wins++;
+    guessedLetters.length = 0;
 
-   var html = "<h1>Psychic Game</h1>" +
-   "<h4>Guess what letter I'm thinking of</h4>" + 
-   "<h3>Wins: " + wins + "</h3>"+ 
-   "<h3>Losses: "+ losses + "</h3>" +
-   "<h3>Guesses left: " + guessesLeft + "</h3>" +
-   "<p>Your guesses so far: " +  guessedLetters; + "</p>";
 
-    document.querySelector("#psychicGame").innerHTML = html;
-    }
+} else if (guessesLeft === 0) {
+    losses++;
+    reset();
+    guessesLeft = 12;
 
+
+} else if (userguess !== currentLetter) {
+    guessesLeft--;
+
+
+}
+
+
+function reset() {
+    guessesLeft = 12;
+    guessedLetters = [];
+
+}
+
+var html = "<h1>Psychic Game</h1>" +
+    "Guess what letter I'm thinking of" +
+    "<h3>Wins: " + wins + "</h3>" +
+    "<h3>Losses: " + losses + "</h3>" +
+    "<h3>Guesses left: " + guessesLeft + "</h3>" +
+    "<p>Your guesses so far: " + guessedLetters; + "</p>";
+
+document.querySelector("#psychicGame").innerHTML = html;
+
+}
